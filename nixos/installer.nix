2b106131a52f401c4bdbe4cd2c7632e4ea9808cc
June Rockway <2367+jrockway@users.noreply.github.com>
2025-06-09 22:23:39 -0400

    nix installer: let me run dmesg without sudo...

--- nixos/installer.nix
+++ nixos/installer.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
+  boot.kernel.sysctl = { "kernel.dmesg_restrict" = 0; };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [ mg pciutils mmc-utils usbutils ];
 }
