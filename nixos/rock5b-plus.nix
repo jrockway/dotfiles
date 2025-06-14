683756b223d4636a1415e832ec85862faba6ff87
June Rockway <2367+jrockway@users.noreply.github.com>
2025-06-14 04:40:33 -0400

    fix chrony config; hopefully make rtc work properly

--- nixos/rock5b-plus.nix
+++ nixos/rock5b-plus.nix
@@ -1,5 +1,12 @@
-{ config, lib, pkgs, ... }: {
-  services.chrony.extraConfig = "makestep 0.1 3";
+{ ... }: {
+  services.chrony = {
+    initstepslew.enabled = false; # makestep is better
+    enableRTCTrimming = false;
+    extraConfig = ''
+      makestep 0.1 3
+      rtcsync
+    '';
+  };
   systemd.network.networks."enP4p65s0" = {
     matchConfig.Name = "enP4p65s0";
     networkConfig.DHCP = "ipv4";
