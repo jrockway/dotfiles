9a98e3e695bff09c9ac9b73512343424b3b04eab
June Rockway <2367+jrockway@users.noreply.github.com>
2026-04-15 23:55:00 -0400

    cleanup home-manager config

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -20,21 +20,7 @@ in {
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
     pkgs.aspell
     pkgs.aspellDicts.en
     pkgs.aspellDicts.en-computers
@@ -252,9 +238,7 @@ in {
     emacs = {
       enable = true;
       package = emacs;
-      extraPackages = e:
-        let
-        in [
+      extraPackages = e: [
           e.bazel
           e.clang-format
           e.claude-code
